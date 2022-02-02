05d2eba717f79504c8ada427eacdfdfb4c7cf593
Gopal Thakur <gopal04thakur@gmail.com>
2022-02-02 13:57:35 +0530

    add templates and static

--- client_front/views.py
+++ client_front/views.py
@@ -1,6 +1,15 @@
+from re import template
+from unittest import loader
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi this is demo")
+    template = loader.get_template('client_front/index.html')
+    context = { 'name':'Mahesh'}
+    return HttpResponse(template.render(context,request))
+def register(request):
+    template = loader.get_template('client_front/register.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
